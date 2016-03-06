5cecf9cb21bda2c29008ee0ca2ba04a433fe3481
Elton Viana <eltonvs@outlook.com>
2016-03-06 16:11:24 -0300

    Initial commit (with the project structure and random fill)

---
+++ include/search.h
@@ -0,0 +1,10 @@
+#ifndef _COMPACT_H_
+#define _COMPACT_H_
+
+#include <vector>
+
+// Function prototypes.
+
+#include "search.inl"
+
+#endif
---
+++ src/drive_analisis.cpp
@@ -0,0 +1,89 @@
+// Copyright - 2016
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <random>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <algorithm>
+#include <functional>
+#include <limits>
+
+#include "search.h"
+
+//
+// Constant expressions.
+//
+constexpr auto N_DEFAULT(20u);  // Default number of elements.
+constexpr auto N_MAX(31u);     // Max number of elements in the array.
+
+//
+// Function prototypes.
+//
+template <typename T>
+void randomFill(T *&, const T, const T, const unsigned int, const int);
+
+//
+// Main program.
+//
+int main(int argc, char *argv[]) {
+    auto arrSz(N_DEFAULT);  // Initialize vector size.
+
+    // Process any command line arguments.
+    if (argc > 1) {
+    // In case the user has provided a value for us.
+        std::stringstream(argv[1]) >> arrSz;
+    }
+
+    // Do we have a valid array size so far?
+    if (arrSz <= 5 || arrSz > N_MAX) {
+        std::cout << "\n\n>>> Invalid argument provided!\n"
+                  << "    Correct sintax: " << argv[0] << " [N > 5 and N <= " << N_MAX << "]\n"
+                  << "    Assuming N = " << N_DEFAULT << " for this run.\n";
+        arrSz = N_DEFAULT;  // Back to the default value.
+    }
+
+    std::cout << "\n\n>>> Required array size is: 2^" << arrSz << "\n\n";
+
+    // Store the data.
+    long int *V, l = 0, u = 100;
+    V = new long int[arrSz];
+
+    // Seed with a real random value, if available.
+    std::random_device r;
+    // Fill it up with random integers.
+    randomFill(V, l, u, r(), arrSz);
+
+    // Printing out the array, just to make sure we've got random integers.
+    std::cout << ">>> ORIGINAL Vet = [ ";
+    for (auto i(0u); i < arrSz; i++)
+        std::cout << *(V+i) << ' ';
+    std::cout << "], Size = " << arrSz << "\n\n";
+
+    // Sort array.
+    std::sort(V, V + arrSz);
+
+    // Printing out the sorted array.
+    std::cout << ">>> SORTED Vet = [ ";
+    for (auto i(0u); i < arrSz; i++)
+        std::cout << *(V+i) << ' ';
+    std::cout << "], Size = " << arrSz << "\n\n";
+
+    delete[] V;
+
+    return EXIT_SUCCESS;
+}
+
+// Fill a vector with random numbers in the range [l -> lower, u -> upper]
+template <typename T>
+void randomFill(T *&V, const T l, const T u, const unsigned int seed, const int s) {
+    // use the default random engine and an uniform distribution
+    std::default_random_engine eng(seed);
+    std::uniform_real_distribution<double> distr(l, u);
+
+    // Fill up vector
+    for (int i = 0; i < s; i++)
+        *(V+i) = distr(eng);
+}
