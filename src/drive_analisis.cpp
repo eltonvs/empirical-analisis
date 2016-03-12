00375c00c93102dbcc7f6a1e734bdd2e75463154
Elton Viana <eltonvs@outlook.com>
2016-03-12 17:41:57 -0300

    Comment std::cout's to make console more clear

--- src/drive_analisis.cpp
+++ src/drive_analisis.cpp
@@ -92,9 +92,12 @@ int main(int argc, char *argv[]) {
                 continue;
 
             std::cout << ">>> Processing " << functions_name[i] << '\n';
-            std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
-            std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
-            std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
+            //std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
+            //std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
+            //std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
+            time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1);
+            time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1);
+            time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]);
         }
     }
 
@@ -115,15 +118,21 @@ int main(int argc, char *argv[]) {
                 continue;
 
             std::cout << ">>> Processing " << functions_name[i] << '\n';
-            std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
-            std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
-            std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
+            //std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
+            //std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
+            //std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
+            time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1);
+            time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1);
+            time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]);
         }
         for (int i = 3; i < 8; i++) {
             std::cout << ">>> Processing " << functions_name[i] << '\n';
-            std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
-            std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
-            std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, sorted_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
+            //std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
+            //std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
+            //std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, sorted_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
+            time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1);
+            time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1);
+            time_measurement_nth(N_EXEC, sorted_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]);
         }
     }
 
