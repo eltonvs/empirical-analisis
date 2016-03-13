af84cad287bf1a48d7ca29408666f67902da2434
Elton Viana <eltonvs@outlook.com>
2016-03-13 15:15:22 -0300

    Fix \n e \t

--- src/drive_analisis.cpp
+++ src/drive_analisis.cpp
@@ -130,18 +130,18 @@ int main(int argc, char *argv[]) {
             //std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
             //std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
             //std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
-            files[i][0] << n << "/t" << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << "/n";
-            files[i][1] << n << "/t" << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << "/n";
-            files[i][2] << n << "/t" << time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << "/n";
+            files[i][0] << n << "\t" << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << "\n";
+            files[i][1] << n << "\t" << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << "\n";
+            files[i][2] << n << "\t" << time_measurement_nth(N_EXEC, mixed_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << "\n";
         }
         for (int i = 3; i < 8; i++) {
             std::cout << ">>> Processing " << functions_name[i] << '\n';
             //std::cout << std::fixed << "WC = " << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << '\n';
             //std::cout << std::fixed << "TQ = " << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << '\n';
             //std::cout << std::fixed << "KE = " << time_measurement_nth(N_EXEC, sorted_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << '\n';
-            files[i][0] << n << "/t" << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << "/n";
-            files[i][1] << n << "/t" << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << "/n";
-            files[i][2] << n << "/t" << time_measurement_nth(N_EXEC, sorted_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << "/n";
+            files[i][0] << n << "\t" << time_measurement(N_EXEC, functions[i], worst_case, V.begin(), 0, n-1) << "\n";
+            files[i][1] << n << "\t" << time_measurement(N_EXEC, functions[i], third_quartile, V.begin(), 0, n-1) << "\n";
+            files[i][2] << n << "\t" << time_measurement_nth(N_EXEC, sorted_search_nth, third_quartile, V.begin(), 0, n-1, 2, functions[i]) << "\n";
         }
     }
 
